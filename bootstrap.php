70b6c595b011aa064acf7a399210a28e6f0487c0
tourze <25803471@qq.com>
2015-08-21 16:48:17 +0800

    update

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,6 @@
 <?php
 
 use tourze\Base\Config;
-use tourze\Base\I18n;
-use tourze\Base\Message;
-use tourze\View\View;
 
 // 指定配置加载目录
 Config::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
